d73bf57a87b00928621478518c49b1575559e465
97SUN <kswoo97241124@gmail.com>
2025-09-22 15:30:13 +0900

    UI_Timer

---
+++ Source/CatheralBattle/Private/TimeAttackGameMode.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeAttackGameMode.h"
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TimeAttackGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+ATimeAttackGameMode::ATimeAttackGameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATimeAttackGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	TimeRemaining = InitialTime; // 시작할 때 남은 시간을 초기값으로 세팅 오류방지~ 
+}
+
+void ATimeAttackGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (TimeRemaining > 0.f)
+	{
+		TimeRemaining -= DeltaSeconds;
+
+		if (TimeRemaining <= 0.f)
+		{
+			TimeRemaining = 0.f;
+			UE_LOG(LogTemp, Warning, TEXT("The time has come"));
+			// 때가 되었다 .. 라는 의미의 문구 썼어요
+		}
+	}
+}
---
+++ Source/CatheralBattle/Public/TimeAttackGameMode.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TimeAttackGameMode.generated.h"
+
+UCLASS()
+class CATHERALBATTLE_API ATimeAttackGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATimeAttackGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	// 시작 시간 (기본값 120초)
+	UPROPERTY(EditAnywhere, Category = "Timer")
+	float InitialTime = 60.f;
+
+	// 남은 시간
+	float TimeRemaining;
+
+public:
+	// 남은 시간 가져오기 (UI에서 접근 가능)
+	UFUNCTION(BlueprintCallable, Category = "Timer")
+	float GetTimeRemaining() const { return TimeRemaining; }
+};
