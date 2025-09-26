ea077c05b95968207db50d34758f5684c25c4e57
leegiman911 <59815475+leegiman911@users.noreply.github.com>
2025-09-26 11:09:32 +0900

    Make CurrentHP editable in Monster class
    Changed the CurrentHP property in Monster.h from VisibleAnywhere, BlueprintReadOnly to EditAnywhere, BlueprintReadWrite, allowing it to be edited in the editor and via Blueprints. Updated related assets to support this change.

--- Source/CatheralBattle/Public/Monster.h
+++ Source/CatheralBattle/Public/Monster.h
@@ -31,7 +31,7 @@ public:
         UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
         float MaxHP = 25.f; //최대체력
 
-        UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stats")
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
         float CurrentHP; //현제 채력
 
         UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
